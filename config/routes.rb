3bcd4b2e1a541d740e1c5445cc29d1e2c44259e2
Kirsty Leishman <leishman.kirsty@gmail.com>
2018-02-21 15:12:30 +0000

    first version done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # GET "restaurants"
+
+  # GET "restaurants/new"
+  # POST "restaurants"
+
+  # GET "restaurants/:id"
+
+  # GET "restaurants/:id/reviews/new"
+  # POST "restaurants/:id/reviews"
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
